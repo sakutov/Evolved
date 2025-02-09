ae41b9fb853bead2b9b44f147ad3ced94b7c27a1
HentaikaZ <zanegrds@gmail.com>
2025-02-09 15:08:55 +0300

    Update EVOLVED TEST.lua

--- EVOLVED TEST.lua
+++ EVOLVED TEST.lua
@@ -883,6 +883,8 @@ end
 
 function sampev.onSendSpawn()
     newTask(function()
+        wait(11111)
+        tp(1011.48, -2242.29, 13.03)
         wait(959362)
         if cfg.main.runspawn == 1 then
             teleportToRandomLocation()
